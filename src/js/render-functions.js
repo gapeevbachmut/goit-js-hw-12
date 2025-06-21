a1baa99828a3224219ad24f51ee3372dface0f08
Андрій <gapeev1979@gmail.com>
2025-06-21 17:03:31 +0300

    fix totalPages, finally клік

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,19 +1,9 @@
-// У файлі render-functions.js створи екземпляр SimpleLightbox для роботи з модальним вікном та зберігай функції для відображення елементів інтерфейсу:
-
-// createGallery(images). Ця функція повинна приймати масив images, створювати HTML-розмітку для галереї, додавати її в контейнер галереї та викликати метод екземпляра SimpleLightbox refresh(). Нічого не повертає.
-// clearGallery(). Ця функція нічого не приймає та повинна очищати вміст контейнера галереї. Нічого не повертає.
-// showLoader(). Ця функція нічого не приймає, повинна додавати клас для відображення лоадера. Нічого не повертає.
-// hideLoader(). Ця функція нічого не приймає, повинна прибирати клас для відображення лоадера. Нічого не повертає.
-// showLoadMoreButton(). Ця функція нічого не приймає, повинна додавати клас для відображення кнопки Load more. Нічого не повертає.
-// hideLoadMoreButton(). Ця функція нічого не приймає, повинна прибирати клас для відображення кнопки Load more. Нічого не повертає.
-
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const list = document.querySelector('.list');
 const loader = document.querySelector('.loader');
 
-// додам кнопку more ДЗ №12
 export const more = document.querySelector('.btn-more');
 
 const lightbox = new SimpleLightbox('.gallery a', {
@@ -71,8 +61,8 @@ export function createGallery(images) {
     )
     .join('');
 
-  list.insertAdjacentHTML('beforeend', markup); // вставляємо розмітку в ul.list
-  lightbox.refresh(); // оновлюємо SimpleLightbox, щоб бачив нові <a>
+  list.insertAdjacentHTML('beforeend', markup);
+  lightbox.refresh();
 }
 
 export function clearGallery() {
@@ -87,8 +77,6 @@ export function hideLoader() {
   loader.classList.add('hidden');
 }
 
-// ДЗ №12 ховаємо та показуємо кнопку more
-
 export function showLoadMoreButton() {
   more.classList.remove('hidden');
 }
@@ -96,9 +84,3 @@ export function showLoadMoreButton() {
 export function hideLoadMoreButton() {
   more.classList.add('hidden');
 }
-
-///////////////////////////
-// не відображаються на гіте
-// <svg width="10" height="10">
-//   <use href="../img/icons.svg#icon-likes"></use>
-// </svg>
--- src/main.js
+++ src/main.js
@@ -1,35 +1,30 @@
-// У файлі main.js напиши всю логіку роботи додатка. Виклики нотифікацій iziToast, усі перевірки на довжину масиву в отриманій відповіді та логіку прокручування сторінки (scroll) робимо саме в цьому файлі. Імпортуй в нього функції із файлів pixabay-api.js та render-functions.js та викликай їх у відповідний момент.
-
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
-import {
-  getImagesByQuery, //  запит на сервер
-} from './js/pixabay-api.js';
+import { getImagesByQuery } from './js/pixabay-api.js';
 
 import {
-  createGallery, // створення розмітки
-  clearGallery, // видалення розмітки
-  showLoader, // показати завантаження
-  hideLoader, // приховати завантаження
+  createGallery,
+  clearGallery,
+  showLoader,
+  hideLoader,
   more,
   showLoadMoreButton,
   hideLoadMoreButton,
 } from './js/render-functions.js';
 
+let myQuery;
 let page = 1;
 let per_page = 15;
+let totalPages = 0;
 
 const form = document.querySelector('.form');
 const input = document.querySelector('input');
 
 form.addEventListener('submit', handleSubmit);
-
 more.addEventListener('click', moreClick);
 
-let myQuery;
-
-//                                              сабміт
+//                                                      сабміт
 async function handleSubmit(event) {
   event.preventDefault();
 
@@ -44,33 +39,31 @@ async function handleSubmit(event) {
     });
     return;
   }
-  page = 1; // обнуляю нумерацію сторінок при новому сабміті
-  clearGallery(); //
-  showLoader(); // показати завантаження
+  page = 1;
+  clearGallery();
+  showLoader();
 
   try {
     const data = await getImagesByQuery(request, page);
-    console.log('sub', page);
 
     if (data.hits.length === 0) {
-      //якщо пустий масив
       iziToast.error({
         title: '❌ Error',
         message:
           'Вибачте, немає зображень, які відповідають вашому пошуковому запиту. Спробуйте ще раз!',
         position: 'topRight',
       });
-      hideLoadMoreButton(); // якщо нічого не знайшли, кнопку теж сховати
+      hideLoadMoreButton();
       return;
     }
 
+    totalPages = Math.ceil(data.totalHits / per_page);
     createGallery(data.hits);
 
-    // перевіряємо, якщо результатом запиту може бути і одне зображення
-    if (data.hits.length < per_page) {
-      hideLoadMoreButton(); // ховаємо кнопку
+    if (page >= totalPages) {
+      hideLoadMoreButton();
     } else {
-      showLoadMoreButton(); //  - показати кнопку
+      showLoadMoreButton();
     }
   } catch (error) {
     console.error(error);
@@ -80,29 +73,25 @@ async function handleSubmit(event) {
       position: 'topRight',
     });
   } finally {
-    hideLoader(); // приховати завантаження
-
-    form.reset(); // очистити пр новому інпуті
+    hideLoader();
+    form.reset();
   }
 }
 
-//                                  клік
+//                                               клік
 async function moreClick(event) {
   event.preventDefault();
 
   page += 1;
-  more.disabled = true; // кнопка не активна
-  showLoader(); // показати завантаження
+  more.disabled = true;
+  showLoader();
   hideLoadMoreButton();
 
   try {
     const moreImages = await getImagesByQuery(myQuery, page);
 
-    console.log(myQuery);
+    createGallery(moreImages.hits);
 
-    createGallery(moreImages.hits); // відмальовуємо розмітку
-
-    const totalPages = Math.ceil(moreImages.totalHits / per_page);
     if (page >= totalPages) {
       hideLoadMoreButton();
 
@@ -112,16 +101,10 @@ async function moreClick(event) {
         position: 'topRight',
       });
     } else {
-      more.disabled = false; //активна тільки якщо ще є сторінки - ???
+      more.disabled = false;
+      showLoadMoreButton();
     }
 
-    console.log(moreImages);
-    console.log(page);
-    console.log(moreImages.totalHits);
-
-    // more.disabled = false; // кнопка активна - ???
-    hideLoader(); // приховати завантаження
-    showLoadMoreButton();
     const card = document.querySelector('.gallery-item');
 
     if (card) {
@@ -139,5 +122,7 @@ async function moreClick(event) {
       message: 'Щось пішло не так. Спробуйте ще раз.',
       position: 'topRight',
     });
+  } finally {
+    hideLoader();
   }
 }
